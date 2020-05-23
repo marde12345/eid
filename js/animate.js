4d23afb55944b4e1fb60dade3cf9af741451faf3
Marde Fasma <mardefasma123up@gmail.com>
2020-05-23 18:59:03 +0700

    init

---
+++ js/animate.js
@@ -0,0 +1,117 @@
+function animate(typewriter) {
+    sequence = [
+        {
+            text: "root@fasma:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "Assalamu ‘alaikum wa rahmatullahi wa barakatuh,\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@fasma:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "Saya Marde Fasma mengucapkan...\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@fasma:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "Selamat Hari Raya Idul Fitri 1441 Hijriah.\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@fasma:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "Serta di hari kemenangan, hari yang suci dan penuh berkah ini,\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@fasma:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "jika saya pernah berbuat salah baik dari perilaku maupun...\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@fasma:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "perkataan kepada kalian, maka ingatlah bahwa itu semua...\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@fasma:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "MEMANG PANTAS UNTUK KALIAN BANGS",
+            delayAfter: 1000
+        },{
+            text: "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\badalah benar-benar kekhilafan saya.\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@fasma:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "Oleh karena itu, saya atas nama pribadi juga ingin meminta...\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@fasma:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "maaf sebesar-besarnya untuk kekhilafan-kekhilafan...\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@fasma:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "yang pernah dilakukan.\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@fasma:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "Minal Aidzin Wal Faidzin.\n",
+            delayAfter: 500
+        },
+        {
+            text: "root@fasma:~# ",
+            instant: true,
+            delayAfter: 500
+        },
+        {
+            text: "Wassalamu 'alaikum wa rahmatullahi wa barakatuh.\n",
+            delayAfter: 500
+        },
+    ];
+
+    typewriter.playSequence(sequence);
+}
